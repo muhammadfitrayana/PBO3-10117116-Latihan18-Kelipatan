c3be15ae42225a08da4c4f81de58eca1816e0b8b
muhammadfitrayana <38598539+muhammadfitrayana@users.noreply.github.com>
2018-10-01 21:21:02 +0700

    Latihan18

---
+++ PBO3-10117116-Latihan18-Kelipatan/src/pbo3/pkg10117116/latihan18/kelipatan/PBO310117116Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117116.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA  : Muhammad Fitrayana
+ * Kelas : PBO3
+ * NIM   : 10117116
+ * Deskripsi Tugas : Membuat Program Kelipatan
+ *  
+ */
+public class PBO310117116Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        for (Double i = 3.5; i<=35; i=i+3.5) {
+        System.out.println(i);    
+        }        
+        
+    }
+}        
+        
+    
+    
+
